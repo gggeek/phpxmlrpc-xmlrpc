e5bafafc09c099407854dd10ddba414f8192baca
gggeek <giunta.gaetano@gmail.com>
2017-11-14 23:51:50 +0000

    work

--- src/Core/Request.php
+++ src/Core/Request.php
@@ -6,6 +6,14 @@ use PhpHttpRpc\Core\Request as BaseRequest;
 
 class Request extends BaseRequest
 {
+    protected $contentType = 'text/xml';
+
+    public function __construct($methodName, array $params = array())
+    {
+        // strip out param names, since xmlrpc only uses positional params
+        parent::__construct($methodName, array_keys($params));
+    }
+
     public function getHTTPBody()
     {
         /// @todo
